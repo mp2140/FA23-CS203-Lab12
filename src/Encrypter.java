973d875066ca11422a77790f10ea74e71e18d136
Mary Arden Pennington <145732498+mp2140@users.noreply.github.com>
2023-11-14 16:42:38 -0600

    final

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -12,12 +12,13 @@ public class Encrypter {
      * Default Constructor
      */
     public Encrypter() {
-        this.shift = 1;
+        this.shift = 0;
         this.encrypted = "";
     }
 
     /**
      * Non-default Constructor
+     *
      * @param s - custom shift amount
      */
     public Encrypter(int s) {
@@ -30,10 +31,13 @@ public class Encrypter {
      *
      * @param inputFilePath      the path to the file containing the text to be encrypted
      * @param encryptedFilePath the path to the file where the encrypted text will be written
-     * @throws Exception if an error occurs while reading or writing the files
+     * @throws IOException if an error occurs while reading or writing the files
      */
-    public void encrypt(String inputFilePath, String encryptedFilePath) throws Exception {
-        //TODO: Call the read method, encrypt the file contents, and then write to new file
+    public void encrypt(String inputFilePath, String encryptedFilePath) throws IOException {
+        // Call the read method, encrypt the file contents, and then write to a new file
+        String content = readFile(inputFilePath);
+        String encryptedContent = encryptText(content, shift);
+        writeFile(encryptedContent, encryptedFilePath);
     }
 
     /**
@@ -41,10 +45,13 @@ public class Encrypter {
      *
      * @param messageFilePath    the path to the file containing the encrypted text
      * @param decryptedFilePath the path to the file where the decrypted text will be written
-     * @throws Exception if an error occurs while reading or writing the files
+     * @throws IOException if an error occurs while reading or writing the files
      */
-    public void decrypt(String messageFilePath, String decryptedFilePath) throws Exception {
-        //TODO: Call the read method, decrypt the file contents, and then write to new file
+    public void decrypt(String messageFilePath, String decryptedFilePath) throws IOException {
+        // Call the read method, decrypt the file contents, and then write to a new file
+        String encryptedContent = readFile(messageFilePath);
+        String decryptedContent = decryptText(encryptedContent, shift);
+        writeFile(decryptedContent, decryptedFilePath);
     }
 
     /**
@@ -52,12 +59,16 @@ public class Encrypter {
      *
      * @param filePath the path to the file to be read
      * @return the content of the file as a string
-     * @throws Exception if an error occurs while reading the file
+     * @throws IOException if an error occurs while reading the file
      */
-    private static String readFile(String filePath) throws Exception {
-        String message = "";
-        //TODO: Read file from filePath
-        return message;
+    private static String readFile(String filePath) throws IOException {
+        StringBuilder message = new StringBuilder();
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                message.append(scanner.nextLine()).append("\n");
+            }
+        }
+        return message.toString();
     }
 
     /**
@@ -65,9 +76,43 @@ public class Encrypter {
      *
      * @param data     the data to be written to the file
      * @param filePath the path to the file where the data will be written
+     * @throws IOException if an error occurs while writing to the file
      */
-    private static void writeFile(String data, String filePath) {
-        //TODO: Write to filePath
+    private static void writeFile(String data, String filePath) throws IOException {
+        try (FileWriter writer = new FileWriter(new File(filePath))) {
+            writer.write(data);
+        }
+    }
+
+    /**
+     * Encrypts text using a simple Caesar cipher with a shift value.
+     *
+     * @param text  the text to be encrypted
+     * @param shift the shift value for the cipher
+     * @return the encrypted text
+     */
+    private static String encryptText(String text, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+        for (char ch : text.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                encryptedText.append((char) ((ch - base + shift) % 26 + base));
+            } else {
+                encryptedText.append(ch);
+            }
+        }
+        return encryptedText.toString();
+    }
+
+    /**
+     * Decrypts text that was encrypted using a simple Caesar cipher with a shift value.
+     *
+     * @param text  the text to be decrypted
+     * @param shift the shift value for the cipher
+     * @return the decrypted text
+     */
+    private static String decryptText(String text, int shift) {
+        return encryptText(text, 26 - shift); // Decrypting is the same as encrypting with the opposite shift
     }
 
     /**
--- src/EncryptionTester.java
+++ src/EncryptionTester.java
@@ -1,20 +1,24 @@
+import java.io.IOException;
 
 public class EncryptionTester {
-	
-	public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
         String encryptedFilePath = "src/encrypted.txt";
         String decryptedFilePath = "src/decrypted.txt";
         String inputFilePath = "src/encryptMe.txt";
         String outputFilePath = "src/newEncrypted.txt";
-		
-        int shift = 1; // Says how much you would like to shift
+
+        int shift = 4; // Says how much you would like to shift
 
         Encrypter enc = new Encrypter(shift);
 
-		enc.encrypt(inputFilePath, outputFilePath);
-		enc.decrypt(encryptedFilePath, decryptedFilePath);
-
-	}
-
-
+        try {
+            enc.encrypt(inputFilePath, outputFilePath);
+            enc.decrypt(encryptedFilePath, decryptedFilePath);
+            System.out.println("Encryption and decryption completed successfully.");
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
